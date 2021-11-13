2867028ecc763455a1af0dd66329beb3c6d0706c
florencia chao <fchao@itba.edu.ar>
2021-11-13 12:52:41 -0300

    Falta el llamado a los desafios y comportamientos

--- client.c
+++ client.c
@@ -1,5 +1,5 @@
 // Client side C/C++ program to demonstrate Socket programming
-//https://www.geeksforgeeks.org/socket-programming-cc/ idea sacada de aca
+// https://www.geeksforgeeks.org/socket-programming-cc/ idea sacada de aca
 #include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -7,11 +7,16 @@
 #include <string.h>
 #define PORT 8080
 
+void cleanBuffer(char * buffer);
+
 int main(int argc, char const *argv[])
 {
 	int sock = 0, valread;
 	struct sockaddr_in serv_addr;
+    int still_in_game =1;
 	char buffer[1024] = {0};
+
+    ////////// CONEXION AL SERVIDOR   ///////////
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
 		printf("\n Socket creation error \n");
@@ -24,15 +29,36 @@ int main(int argc, char const *argv[])
 	// Convert IPv4 and IPv6 addresses from text to binary form
 	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
 	{
-		printf("\nInvalid address/ Address not supported \n");
+		perror("\nInvalid address/ Address not supported \n");
 		return -1;
 	}
 
 	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
 	{
-		printf("\nConnection Failed \n");
+		perror("\nConnection Failed \n");
 		return -1;
 	}
 
+    printf("Felicitaciones. Lograste una conexion exitosa\n");
+    //////////////////////////////////////////////////
+    
+    while(still_in_game){ //ahora esta como en un while 1. Hay que ver como hacemos que corte cuando termina
+
+        scanf("%s", buffer);
+        strcat(buffer, "\n");
+        send(sock,buffer, strlen(buffer), 0 ); //mandas la respuesta
+        cleanBuffer(buffer);  
+    
+    }
+
+    close(sock);
+
 	return 0;
+}
+
+void cleanBuffer(char * buffer){
+    int i;
+    for(i=0; i< strlen(buffer) || buffer[i]==0; i++){
+        buffer[i] = 0;
+    }
 }
--- server.c
+++ server.c
@@ -1,54 +1,92 @@
 // Server side C/C++ program to demonstrate Socket programming
+// https://www.geeksforgeeks.org/socket-programming-cc/ idea sacada de aca
+
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#define MAX_DESAFIOS 13
+#define CANT_RESPUESTAS 12
+char *rtas[CANT_RESPUESTAS] = {"entendido\n", "itba\n", "M4GFKZ289aku\n", "fk3wfLCm3QvS\n", "too_easy\n", ".RUN_ME\n", "K5n2UFfpFMUN\n", "BUmyYq5XxXGt\n", "u^v n", "chin_chu_lan_cha\n", "gdb_rules\n", "normal\n"};
+
 #define PORT 8080
+
+void cleanBuffer(char * buffer){
+    int i;
+    for(i=0; i< strlen(buffer) || buffer[i]==0; i++){
+        buffer[i] = 0;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
-	int server_fd, new_socket, valread;
-	struct sockaddr_in address;
-	int opt = 1;
-	int addrlen = sizeof(address);
-	char buffer[1024] = {0};
-	
-	// Creating socket file descriptor
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-	{
-		perror("socket failed");
-		exit(EXIT_FAILURE);
-	}
-	
-	// Forcefully attaching socket to the port 8080
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-												&opt, sizeof(opt)))
-	{
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
-	}
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons( PORT );
-	
-	// Forcefully attaching socket to the port 8080
-	if (bind(server_fd, (struct sockaddr *)&address,
-								sizeof(address))<0)
-	{
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
-	if (listen(server_fd, 3) < 0)
-	{
-		perror("listen");
-		exit(EXIT_FAILURE);
-	}
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-					(socklen_t*)&addrlen))<0)
-	{
-		perror("accept");
-		exit(EXIT_FAILURE);
-	}
-	return 0;
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+	char bufferClient[124] = {0};
+    int desafioNro;
+    //////////////////////////// CREACION DEL SERVER ////////////////////////
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                   &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address,
+             sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    ///////////////////////////////////////////////////////////////
+
+    while ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)))
+    {
+        desafioNro = 1;
+        //habria que llamar al primer desafio
+		//(*desafios[desafioNro])();
+        while(desafioNro < MAX_DESAFIOS && (valread = read(new_socket, bufferClient, (size_t) bufferClient))>0){
+            //(*desafios[desafioNro])();
+            if(strcmp(bufferClient, rtas[desafioNro]) == 0){
+                desafioNro++;
+                if(desafioNro<MAX_DESAFIOS){
+                    //(*desafios[desafioNro])();
+                }
+            }
+            else{
+                sprintf(buffer,"Respuesta incorrecta: %s", bufferClient);
+            }
+            cleanBuffer(bufferClient);
+        }
+        if (new_socket < 0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+    }
+    close(server_fd);
+    return 0;
 }
